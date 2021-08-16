d9ec4df5cbf84f99f3e5af2f8bb5df9d947f4d64
jitendradev <jitendra.suthar@growcify.com>
2021-08-16 23:09:13 +0530

    Created 3 new views Cat.vue, Dogs.vue, Pets.vue and added them into router and use them in App.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
+import Pets from '../views/Pets.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -8,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/cats',
+    name: 'Cats',
+    component: Cats
+  },
+  {
+    path: '/dogs',
+    name: 'Dogs',
+    component: Dogs
+  },
+  {
+    path: '/pets',
+    name: 'Pets',
+    component: Pets
   }
 ]
 
---
+++ src/views/Cats.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is Cats component</h1>
+  </div>
+</template>
