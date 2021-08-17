ec8112115ebdb4c26e6ddbfd8a49ccbb54da472b
jitendradev <jitendra.suthar@growcify.com>
2021-08-17 13:10:23 +0530

    added bootstrap-vue to style Vue components. Created tables for Cats and dogs and rendered data from data folder's cats.js and dogs.js file

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>This is Cats component</h1>
+    <h1>Cats for Adoption</h1>
+    <b-table stripped :items="cats"></b-table>
   </div>
 </template>
+
+<script>
+import cats from '@/data/cats'
+
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    <h1>This is Dogs component</h1>
+    <h1>Dogs for Adoption</h1>
+    <b-table stripped :items="dogs"></b-table>
   </div>
 </template>
+
+<script>
+import dogs from '@/data/cats'
+
+export default {
+  data () {
+    return {
+      dogs
+    }
+  }
+}
+</script>
